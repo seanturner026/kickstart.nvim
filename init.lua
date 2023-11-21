03d148cf4ff70ca6af39df8a7846bcf77c99140e
Sean Turner <sean.turner026@gmail.com>
2023-11-20 22:47:49 -0500

    Disable indentation on yaml with treesitter

--- init.lua
+++ init.lua
@@ -487,7 +487,10 @@ vim.defer_fn(function()
     auto_install = true,
 
     highlight = { enable = true },
-    indent = { enable = true },
+    indent = {
+      enable = true,
+      disable = { 'yaml' },
+    },
     incremental_selection = {
       enable = true,
       keymaps = {
