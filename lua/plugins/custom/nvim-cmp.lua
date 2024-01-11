939cf447f7936d4bb25a753014e365dbf6736fa5
Sean Turner <sean.turner026@gmail.com>
2024-01-11 05:48:46 -0500

    Added prettier cmp suggestions / hover

--- lua/plugins/custom/nvim-cmp.lua
+++ lua/plugins/custom/nvim-cmp.lua
@@ -5,20 +5,32 @@ return {
         -- Snippet Engine & its associated nvim-cmp source
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
-
         -- Adds LSP completion capabilities
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-buffer",
-        -- Adds a number of user-friendly snippets
-        "rafamadriz/friendly-snippets",
+        "rafamadriz/friendly-snippets", -- Adds a number of user-friendly snippets
+        "onsails/lspkind.nvim",         -- Required for fancy hover
     },
     config = function()
-        local cmp = require("cmp")
-        cmp.setup({
+        require("cmp").setup({
             window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
+                completion = {
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+                    col_offset = -3,
+                    side_padding = 0,
+                },
+            },
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = function(entry, vim_item)
+                    local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                    kind.kind = " " .. (strings[1] or "") .. " "
+                    kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+                    return kind
+                end,
             },
         })
     end,
