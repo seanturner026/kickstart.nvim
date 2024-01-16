b11b0f309249ec7fcf5027038dd6957f603e76be
Sean Turner <sean.turner026@gmail.com>
2024-01-16 10:56:50 -0500

    Added visual-surround

---
+++ lua/plugins/custom/visual-surround.lua
@@ -0,0 +1,10 @@
+return {
+    "NStefan002/visual-surround.nvim",
+    -- config = function()
+    --     require("visual-surround").setup({
+    --         -- your config
+    --     })
+    -- end,
+    -- or if you don't want to change defaults
+    config = true,
+}
