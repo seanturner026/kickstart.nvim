69abdded18c7654776333d0bf930965fee7c08d9
Sean Turner <sean.turner026@gmail.com>
2024-04-22 10:01:55 -0400

    Changed multi-cursors plugin

--- lua/plugins/custom/vim-visual-multi.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "mg979/vim-visual-multi",
-    lazy = false,
-    config = function()
-        vim.g.VM_maps = {  -- necessary for vim-visual-multi https://github.com/mg979/vim-visual-multi/issues/172#issuecomment-1092293500
-            ["I BS"] = "", -- disable backspace mapping
-        }
-    end,
-}
