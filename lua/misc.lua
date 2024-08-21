9b6b723db98fa836c96ed7349e92e0172d50c01c
Sean Turner <sean.turner026@gmail.com>
2024-08-20 20:09:50 -0400

    Updated with kickstart code

--- lua/misc.lua
+++ lua/misc.lua
@@ -226,13 +226,21 @@ local on_attach = function(client, bufnr)
     end
 
     nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+    -- Execute a code action, usually your cursor needs to be on top of an error or a suggestion from your LSP for
+    -- this to activate.
     nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
     nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+    -- Find references for the word under your cursor.
     nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    -- Jump to the implementation of the word under your cursor.
+    -- Useful when your language has ways of declaring types without an actual implementation.
     nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    -- Jump to the type of the word under your cursor. Useful when you're not sure what type a variable is and you want
+    -- to see the definition of its *type*, not where it was *defined*.
     nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+    -- Fuzzy find all the symbols in your current document (variables, functions, types, etc.).
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    -- Fuzzy find all the symbols in your current workspace.
     nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
     -- See `:help K` for why this keymap
