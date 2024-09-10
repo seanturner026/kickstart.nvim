dda7fe115c2e49c1a63e94387504228eed0a3082
Sean Turner <sean.turner026@gmail.com>
2024-09-10 15:13:00 -0400

    Only wordwrapping with comments

--- lua/options.lua
+++ lua/options.lua
@@ -13,6 +13,7 @@ vim.opt.listchars:append({ trail = "·" })
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.textwidth = 120
+vim.opt.formatoptions = "c"
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
@@ -20,15 +21,15 @@ vim.opt.splitright = true
 vim.opt.inccommand = "split"
 
 -- See `:help vim.o`
-vim.o.hlsearch = false                 -- Set highlight on search
-vim.o.mouse = "a"                      -- Enable mouse mode
-vim.o.clipboard = "unnamedplus"        -- Sync clipboard between OS and Neovim.
-vim.o.breakindent = false              -- Disable break indent
-vim.o.undofile = true                  -- Save undo history
-vim.o.ignorecase = true                -- Case-insensitive searching UNLESS \C or capital in search
+vim.o.hlsearch = false -- Set highlight on search
+vim.o.mouse = "a" -- Enable mouse mode
+vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim.
+vim.o.breakindent = false -- Disable break indent
+vim.o.undofile = true -- Save undo history
+vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.smartcase = true
-vim.wo.signcolumn = "yes"              -- Keep signcolumn on by default
-vim.o.updatetime = 250                 -- Decrease update time
+vim.wo.signcolumn = "yes" -- Keep signcolumn on by default
+vim.o.updatetime = 250 -- Decrease update time
 vim.o.timeoutlen = 300
 vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience
 vim.o.termguicolors = true
