a02f32a096ebcd1c89eee2918d885893368d2731
Sean Turner <sean.turner026@gmail.com>
2024-10-26 09:14:19 -0400

    Removed path

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,8 +2,10 @@
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Easy find and replace.
-vim.keymap.set({ "v" }, "<leader>re", '"hy:%s/<C-r>h/<C-r>h/gc<left><left><left>', { desc = "Open search and replace for currently selected text" })
-vim.keymap.set({ "n" }, "<leader>re", ":%s/<C-r><C-w>/<C-r><C-w>/gc<Left><Left><Left>", { desc = "Open search and replace for word under cursor" })
+vim.keymap.set({ "v" }, "<leader>re", '"hy:%s/<C-r>h/<C-r>h/gc<left><left><left>',
+    { desc = "Open search and replace for currently selected text" })
+vim.keymap.set({ "n" }, "<leader>re", ":%s/<C-r><C-w>/<C-r><C-w>/gc<Left><Left><Left>",
+    { desc = "Open search and replace for word under cursor" })
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gkzz' : 'k'", { expr = true, silent = true })
@@ -43,7 +45,7 @@ vim.keymap.set("n", "dd", function()
 end, { expr = true })
 
 -- Format with Black
-vim.keymap.set("n", "<leader>fmp", ":silent !/Users/sean/.local/bin/black -l 120 --preview --enable-unstable-feature string_processing %<cr>")
+vim.keymap.set("n", "<leader>fmp", ":silent !black -l 120 --preview --enable-unstable-feature string_processing %<cr>")
 
 -- Preserve cursor position when yanking https://nanotipsforvim.prose.sh/sticky-yank
 local cursorPreYank
