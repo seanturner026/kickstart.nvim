09d552b7ae91a55d61840f448c73b87dd8ed892f
Sean Turner <sean.turner026@gmail.com>
2024-10-31 15:52:53 -0400

    UFO configuration

--- lua/plugins/custom/ufo.lua
+++ lua/plugins/custom/ufo.lua
@@ -8,17 +8,18 @@ return {
             end,
         })
 
-        vim.keymap.set("n", "zK", function()
+        vim.keymap.set("n", "zk", function()
             local winid = require("ufo").peekFoldedLinesUnderCursor()
             if not winid then
                 vim.lsp.buf.hover()
             end
         end, { desc = "Peek Fold" })
+
         vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
         vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 
         vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-        vim.o.foldcolumn = "1"
+        vim.o.foldcolumn = "0"
         vim.o.foldenable = true
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
