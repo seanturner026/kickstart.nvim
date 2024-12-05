3e41e4028ff083b2bf8f4ed6975e5499595497fa
Sean Turner <sean.turner026@gmail.com>
2024-12-05 13:51:44 -0500

    Switching back to Oil

--- lua/plugins/custom/mini.files.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "echasnovski/mini.files",
-    version = false,
-    keys = {
-        { "-", "<cmd>lua require('mini.files').open(vim.api.nvim_buf_get_name(0))<CR>", { silent = true }, desc = "Open mini.files" },
-        { "_", "<cmd>lua require('mini.files').open(vim.uv.cwd())<CR>", { silent = true }, desc = "Open mini.files (cwd)" },
-    },
-}
