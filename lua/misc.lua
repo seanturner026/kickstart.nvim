281d2ab55e74ab6fefe97f96fde63621f2f83e79
Sean Turner <sean.turner026@gmail.com>
2025-05-07 08:42:44 -0400

    Keybinds

--- lua/misc.lua
+++ lua/misc.lua
@@ -141,6 +141,8 @@ vim.defer_fn(function()
             "jsonc",
             "lua",
             "make",
+            "markdown",
+            "markdown_inline",
             "python",
             "sql",
             "terraform",
@@ -221,45 +223,42 @@ local on_attach = function(client, bufnr)
         vim.bo.commentstring = "// %s"
         client.server_capabilities.semanticTokensProvider = nil
     end
+
     -- Create a function that lets us more easily define mappings specific for LSP related items. It sets the mode, buffer
     -- and description for us each time.
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
-        end
-
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+    local map = function(keys, func, desc, mode)
+        mode = mode or "n"
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
     end
 
-    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
     -- Execute a code action, usually your cursor needs to be on top of an error or a suggestion from your LSP for
     -- this to activate.
-    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-    nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+    map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+    -- Jump to the definition of the word under your cursor.
+    --  This is where a variable was first declared, or where a function is defined, etc.
+    map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+    -- Execute a code action, usually your cursor needs to be on top of an error
+    -- or a suggestion from your LSP for this to activate.
+    map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+
     -- Find references for the word under your cursor.
-    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
     -- Jump to the implementation of the word under your cursor.
     -- Useful when your language has ways of declaring types without an actual implementation.
-    nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-    -- Jump to the type of the word under your cursor. Useful when you're not sure what type a variable is and you want
-    -- to see the definition of its *type*, not where it was *defined*.
-    nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+    map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
     -- Fuzzy find all the symbols in your current document (variables, functions, types, etc.).
-    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
     -- Fuzzy find all the symbols in your current workspace.
-    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
     -- See `:help K` for why this keymap
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-    -- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation") -- Disabling for tmux navigation
-
-    -- Lesser used LSP functionality
-    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-    nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, "[W]orkspace [L]ist Folders")
+    map("K", vim.lsp.buf.hover, "Hover Documentation")
+    -- map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation") -- Disabling for tmux navigation
 
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
