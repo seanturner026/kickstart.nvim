387607ef4af22ecd565bfa54940b08c10b2ad0d6
Sean Turner <sean.turner026@gmail.com>
2025-05-25 17:30:42 -0400

    New mini-surround bindings to allow leap

--- lua/plugins/custom/mini.lua
+++ lua/plugins/custom/mini.lua
@@ -14,6 +14,19 @@ return {
         -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
-        require("mini.surround").setup()
+        require("mini.surround").setup({
+            mappings = {
+                add = "ma", -- Add surrounding in Normal and Visual modes
+                delete = "md", -- Delete surrounding
+                find = "mf", -- Find surrounding (to the right)
+                find_left = "mF", -- Find surrounding (to the left)
+                highlight = "mh", -- Highlight surrounding
+                replace = "mr", -- Replace surrounding
+                update_n_lines = "mn", -- Update `n_lines`
+
+                suffix_last = "l", -- Suffix to search with "prev" method
+                suffix_next = "n", -- Suffix to search with "next" method
+            },
+        })
     end,
 }
